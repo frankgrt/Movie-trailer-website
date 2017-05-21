a993caccfa2113beda324e7fdd6ab78b6d486ab6
frank grt <frank grt>
2017-05-21 23:28:16 +0800

    Add more movies and comments

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,19 +1,39 @@
 import media
 import fresh_tomatoes
+
+# 6 instance of most liked movies.
 moana = media.Movie("MOANA",
                     "mythic adventure set around 2000 years ago and across a series of islands in the South Pacific",
                     "https://www.tribute.ca/tribute_objects/images/movies/moana/moana-poster-lg.jpg",
                     "https://www.youtube.com/watch?v=LKFuXETZUsI")
 
 sideways = media.Movie("SIDEWAYS",
-                    "A story of a boy and his toys",
+                    "road trip through Santa Ynez Valley wine country",
                     "http://static.rogerebert.com/uploads/movie/movie_poster/sideways-2004/large_tojQbn3H4UcM8lkuns1E7CnLv8D.jpg",
                     "https://www.youtube.com/watch?v=Pqbz8yojxBY")
 
+matrix = media.Movie("MATRIX",
+                    "A computer hacker learns from mysterious rebels about the true nature of his reality and his role in the war against its controllers. ",
+                    "https://neo1210.files.wordpress.com/2010/09/matrix_movie1.jpg",
+                    "https://www.youtube.com/watch?v=m8e-FF8MsqU")
 
+becoming_jane = media.Movie("BECOMING JANE",
+                    "A biographical portrait of a pre-fame Jane Austen and her romance with a young Irishman. ",
+                    "http://www.impawards.com/2007/posters/becoming_jane.jpg",
+                    "https://www.youtube.com/watch?v=qmd-ej9Hx20")
 
+notting_hill = media.Movie("NOTTING HILL",
+                    "The life of a simple bookshop owner changes when he meets the most famous film star in the world. ",
+                    "https://goddessella.files.wordpress.com/2012/03/notting-hill1.jpg",
+                    "https://www.youtube.com/watch?v=4RI0QvaGoiI")
 
+minions = media.Movie("MINIONS",
+                    "Ever since the dawn of time, the Minions have lived to serve the most despicable of masters. ",
+                    "http://www.flickeringmyth.com/wp-content/uploads/2015/06/minions-poster.jpg",
+                    "https://www.youtube.com/watch?v=eisKxhjBnZ0")
 
+# including instance name in a list
+movies = [moana, sideways, matrix,becoming_jane,notting_hill,minions]
 
-movies = [moana, sideways]
+# Create and open html page
 fresh_tomatoes.open_movies_page(movies)
